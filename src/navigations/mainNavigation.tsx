fe90a468ea5a4703aaf369a4fa2378e75b2df7d8
komaldeep chahal <komaldeep.chahal@wee.com>
2021-02-15 10:31:07 +0100

    bring navigation with typecript

--- src/navigations/mainNavigation.tsx
+++ src/navigations/mainNavigation.tsx
@@ -1,8 +1,19 @@
 import React, {FC, useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import firebase from 'firebase';
-import AppStack from './appstack';
-import AuthStack from './authstack';
+import {createStackNavigator} from "@react-navigation/stack";
+import {Dashboard, Home, Login, SignUp} from "../screens";
+
+type RootStackParamList = {
+  home: undefined;
+  Profile: { userId: string };
+  login: undefined;
+  signUp: undefined;
+  dashboard: undefined
+};
+
+
+const RootStack = createStackNavigator<RootStackParamList>();
 
 const MainNav: FC = () => {
   const [user, setUser] = useState<any>(() => {
@@ -25,7 +36,15 @@ const MainNav: FC = () => {
 
   return (
     <NavigationContainer>
-      {user !== null ? <AppStack /> : <AuthStack />}
+      <RootStack.Navigator
+          mode="modal"
+          initialRouteName={user !== null ? "home" : "signUp"}
+      >
+        <RootStack.Screen name="signUp" component={SignUp} />
+        <RootStack.Screen name="login" component={Login} />
+        <RootStack.Screen name="home" component={Home} />
+        <RootStack.Screen name="dashboard" component={Dashboard} />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 };
