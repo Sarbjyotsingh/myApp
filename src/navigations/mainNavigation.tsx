4e62f1746835459ffa2e3551bd2244ffd22ca938
Sarbjyotsingh <chahalsarbjyotsingh@gmail.com>
2021-02-15 20:36:56 +0530

    Animate From Bottom to top in Modal navigation

--- src/navigations/mainNavigation.tsx
+++ src/navigations/mainNavigation.tsx
@@ -1,15 +1,16 @@
 import React, {FC, useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import firebase from 'firebase';
-import {createStackNavigator} from "@react-navigation/stack";
-import {Dashboard, Home, Login, SignUp} from "../screens";
+import {createStackNavigator} from '@react-navigation/stack';
+import {Dashboard, Home, Login, SignUp} from '../screens';
+import {verticalAnimation} from '../utils/utils';
 
 export type RootStackParamList = {
   home: undefined;
-  Profile: { userId: string };
+  Profile: {userId: string};
   login: undefined;
   signUp: undefined;
-  dashboard: undefined
+  dashboard: undefined;
 };
 
 const RootStack = createStackNavigator<RootStackParamList>();
@@ -36,13 +37,28 @@ const MainNav: FC = () => {
   return (
     <NavigationContainer>
       <RootStack.Navigator
-          mode="modal"
-          initialRouteName={user !== null ? "home" : "signUp"}
-      >
-        <RootStack.Screen name="signUp" component={SignUp} />
-        <RootStack.Screen name="login" component={Login} />
-        <RootStack.Screen name="home" component={Home} />
-        <RootStack.Screen name="dashboard" component={Dashboard} />
+        mode="modal"
+        initialRouteName={user !== null ? 'home' : 'signUp'}>
+        <RootStack.Screen
+          name="signUp"
+          component={SignUp}
+          options={verticalAnimation}
+        />
+        <RootStack.Screen
+          name="login"
+          component={Login}
+          options={verticalAnimation}
+        />
+        <RootStack.Screen
+          name="home"
+          component={Home}
+          options={verticalAnimation}
+        />
+        <RootStack.Screen
+          name="dashboard"
+          component={Dashboard}
+          options={verticalAnimation}
+        />
       </RootStack.Navigator>
     </NavigationContainer>
   );
